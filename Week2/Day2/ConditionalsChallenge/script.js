bef86f93ba06df3c1d5b4c54455e91b952aac343
galitpauz <45431987+galitpauz@users.noreply.github.com>
2021-04-13 09:57:14 +0300

    Challenge with other way

--- Week2/Day2/ConditionalsChallenge/script.js
+++ Week2/Day2/ConditionalsChallenge/script.js
@@ -19,3 +19,23 @@ if (wordNot < wordBad){
 else {
     console.log(sentence);
 }
+
+
+// //Other option:
+
+// let sentence = "The movie is not that bad, I like it"
+// let final;
+// let subs;
+
+// let wordNot = sentence.search('not');
+// let wordBad = sentence.search('bad');
+
+// if (wordBad > wordNot && wordNot != -1) {
+//     subs = sentence.substring(wordNot, wordBad +3);
+//     final = sentence.replace(subs, "good");
+
+// } else {
+//     final = sentence;
+// }
+
+// alert(final);
