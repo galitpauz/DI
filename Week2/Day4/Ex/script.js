e4e6ddfa330804764b262500667d62f85e0f2c34
galitpauz <45431987+galitpauz@users.noreply.github.com>
2021-04-16 19:01:12 +0300

    XP

---
+++ Week2/Day4/Ex/script.js
@@ -0,0 +1,30 @@
+// 1. Create a structured html file linked to a js file
+// 2. Write a JS function that takes a parameter: myAge
+// 3. Console.log the age of my mum and my dad (my mum is twice my age, and my dad is 1.2 the age of my mum)
+// 4. Call the 
+
+// function parentAge(myAge) {
+//     let mumAge = myAge*2
+//     let dadAge = mumAge*1.5
+// console.log("my mum is: " + mumAge + " my dad: " + dadAge)
+// }
+
+// parentAge(21);
+
+// function mumAge(myAge) {
+//     let mAge = myAge*2
+// return "my mum is: " + mAge;
+// // console.log("my mum is: " + mumAge)
+// }
+
+// console.log(mumAge(30));
+
+const user = (username, password) => username + password;
+ //same as function
+ function user1(username, password) {
+     return username + password;
+ }
+
+ console.log(user1("rony", "****"))
+ console.log(user("eli", "****"))
+
