3690c3ba546f57bde18a5c7363c5ee6f71ebd448
galitpauz <45431987+galitpauz@users.noreply.github.com>
2021-04-19 11:58:53 +0300

    A better way for the exercise

--- Week2/Day5/99Bottles/script.js
+++ Week2/Day5/99Bottles/script.js
@@ -1,17 +1,38 @@
-function numBeers(maxBeer){
-    maxBeer = parseInt(prompt("Enter a number to begin counting down bottles: "))
-    let minBeer = 1++
-    while (maxBeer >=1){
-        if (maxBeer === 2) {
-        console.log(maxBeer +" bottles of beer on the wall" + maxBeer +"bottles of beer. Take one down pass it around..." + (maxBeer-minBeer)+ " bottle of beer on the wall")
-        } else if (maxBeer === 1) {
-        console.log(maxBeer +" bottle of beer on the wall" + maxBeer +"bottles of beer. Take one down pass it around..." + (maxBeer-minBeer)+ " bottle of beer on the wall")
-        } else {
-        console.log(maxBeer +" bottles of beer on the wall" + maxBeer +"bottles of beer. Take one down pass it around..." + (maxBeer-minBeer)+ " bottles of beer on the wall")
-        maxBeer --;
-        }
-    }
-    
-}
+let maxBeer = parseInt(prompt("Enter a number to begin counting down bottles: "));
+let dropBeer = maxBeer;
 
-numBeers()
+for (i=1; i<=dropBeer; i++){
+    console.log(dropBeer +" bottles of beer on the wall" + dropBeer +"bottles of beer. ")
+    if(i===1){
+        console.log(`Take ${i} down, pass it around`);
+    }
+    else {
+        console.log(`Take ${i} down, pass it around`);
+    }
+    dropBeer -=i;
+    console.log(`${dropBeer} bottle of beer on the wall `);
+}
+console.log("Drink Up")
+
+
+
+
+
+
+// function numBeers(maxBeer){
+//     maxBeer = parseInt(prompt("Enter a number to begin counting down bottles: "))
+//     let minBeer = 1++
+//     while (maxBeer >=1){
+//         if (maxBeer === 2) {
+//         console.log(maxBeer +" bottles of beer on the wall" + maxBeer +"bottles of beer. Take one down pass it around..." + (maxBeer-minBeer)+ " bottle of beer on the wall")
+//         } else if (maxBeer === 1) {
+//         console.log(maxBeer +" bottle of beer on the wall" + maxBeer +"bottles of beer. Take one down pass it around..." + (maxBeer-minBeer)+ " bottle of beer on the wall")
+//         } else {
+//         console.log(maxBeer +" bottles of beer on the wall" + maxBeer +"bottles of beer. Take one down pass it around..." + (maxBeer-minBeer)+ " bottles of beer on the wall")
+//         maxBeer --;
+//         }
+//     }
+    
+// }
+
+// numBeers()
