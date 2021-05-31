7b575307cb81e5f6a3818a4e267b38e77bd690b4
galitpauz <45431987+galitpauz@users.noreply.github.com>
2021-05-31 14:32:45 +0300

    day 1 daily

---
+++ Week8/Day1/daily/src/Carousel.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+
+
+export default () => (
+  <Carousel >
+        {/* <ul>
+      <li>     
+        <div>
+        <img alt="" src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg" />
+        <p className="legend">Hong Kong</p>
+      </div>
+      </li>
+      <li>
+      <div>
+        <img alt="" src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp" />
+        <p className="legend">Legend 2</p>
+      </div>
+      </li>
+      <li>
+      <div>
+        <img alt="" src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp" />
+        <p className="legend">Legend 3</p>
+      </div>
+      </li>
+      <li>
+      <div>
+        <img alt="" src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp" />
+        <p className="legend">Legend 4</p>
+      </div>
+      </li>
+    </ul> */}
+
+
+  </Carousel>
+);
